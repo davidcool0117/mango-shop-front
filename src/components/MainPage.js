018f74b2736f3fedf0f03201a9969e55e0104561
sohnjoongwon <103242419+sohnjoongwon@users.noreply.github.com>
2023-03-13 14:32:04 +0900

    업로드 페이지 구현

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
+import { Link } from 'react-router-dom';
 import "./MainPage.css";
 import axios from "axios";
 
 const MainPage = () => {
     const [products, setProducts] = useState([]);
     useEffect(() => {
-        let url = "https://e20fed74-baa5-45c0-8f03-17b0b3272edd.mock.pstmn.io/products";
+        let url = "https://e20fed74-baa5-45c0-8f03-17b0b3272edd.mock.pstmn.io/products/";
         axios
             .get(url)
             .then((result) => {
@@ -18,36 +19,28 @@ const MainPage = () => {
     }, [])
     return (
         <div>
-            <div id="header">
-                <div id="header-area"><img src="images/icons/logo.png" alt="" /></div>
-            </div>
             <div id="body">
                 <div id="banner"><img src="images/banners/banner1.png" alt="" /></div>
                 <h1>Products</h1>
                 <div id="product-list">
-                    {products.map((product,idx)=>{
-                        return(
-                        <div className="product-card" key={idx}>
-                            <div><img className="product-img" src={product.imageUrl} alt="" /></div>
-                            <div className="product-content">
-                                <span className="product-name">{product.name}</span>
-                                <span className="product-price">{product.price}</span>
-                                <span className="product-seller">
-                                    <img src="images/icons/avatar.png" alt="" className="product-avatar" />
-                                    <span>{product.seller}</span>
-                                </span>
+                    {products.map((product, idx) => {
+                        return (
+                            <div className="product-card" key={idx}>
+                                <Link className="product-link" to={`/ProductPage/${product.id}`}>
+                                    <div><img className="product-img" src={product.imageUrl} alt="product-img" /></div>
+                                    <div className="product-content">
+                                        <span className="product-name">{product.name}</span>
+                                        <span className="product-price">{product.price}</span>
+                                        <span className="product-seller">
+                                            <img src="images/icons/avatar.png" alt="product-seller" className="product-avatar" />
+                                            <span>{product.seller}</span>
+                                        </span>
+                                    </div>
+                                </Link>
                             </div>
-                        </div>
                         )
                     })}
                 </div>
-            </div>
-            <div id="footer">
-                <a href="#">회사소개</a>
-                <a href="#">이용약관</a>
-                <a href="#">통신판매업신고번호:123-1234</a>
-                <a href="#">사업자등록번호:456-56-78951</a>
-                <a href="#">고객센터:456-78951</a>
             </div>
         </div>
     );
---
+++ src/components/UploadPage.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Form, Input, Button, Upload, Divider, InputNumber } from "antd";
+import './UploadPage.css';
+const { TextArea } = Input;
+
+function UploadPage() {
+  const onFinish = (val) => {
+    console.log(val);
+  }
+  return (
+    <div id="upload-container">
+      <Form name='uploadForm' onFinish={onFinish}>
+        <Form.Item name="upload">
+          <div id="upload-img">
+            <img src='/images/icons/camera.png' alt='' />
+            <span>이미지를 업로드 해주세요</span>
+          </div>
+        </Form.Item>
+        <Divider />
+        <Form.Item label={<div className="upload-label">판매자명</div>} name="product-seller" rules={[{ required: true, message: "판매자명은 필수 입력 사항입니다." }]}>
+          <Input className="upload-name" size="large" placeholder="이름을 입력해주세요" />
+        </Form.Item>
+        <Divider />
+        <Form.Item label={<div className="upload-label">상품명</div>} name="product-name" rules={[{ required: true, message: '상품명은 필수 입력 사항입니다.' }]}>
+          <Input className="upload-name" size="large" placeholder="상품명을 입력해주세요" />
+        </Form.Item>
+        <Divider />
+        <Form.Item label={<div className="upload-price">판매가</div>} name="product-price" rules={[{ required: true, message: '판매가는 필수 입력 사항입니다.' }]}>
+          <InputNumber className="upload-price" size="large" min={0} defaultValue={0} />
+        </Form.Item>
+        <Divider />
+        <Form.Item label={<div className="upload-label">상품설명</div>} name="product-description" rules={[{ required: true, message: '상품설명은 필수 입력 사항입니다.' }]}>
+          <TextArea size="large" id="product-description" showCount maxLength={300} placeholder="상품설명을 작성해주세요" />
+        </Form.Item>
+        <Form.Item>
+					<Button id="submit-button" size="large" htmlType="submit">
+						상품등록하기
+					</Button>
+				</Form.Item>
+      </Form>
+    </div>
+  )
+}
+
+export default UploadPage;
