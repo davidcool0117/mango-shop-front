49c5a7fb4a3bb79895a3fdbdf040f0938a25c88c
davidcool0117 <davidcool0117@gmail.com>
2023-03-20 14:51:39 +0900

    업로드 페이지 완료

--- src/App.js
+++ src/App.js
@@ -1,44 +1,48 @@
-import React from 'react';
-import { Routes, Route, Link, useNavigate } from 'react-router-dom';
-import { Button } from 'antd';
-import 'antd/dist/reset.css';
+import React from "react";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import { Button } from "antd";
+import "antd/dist/reset.css";
 import "./App.css";
-import MainPage from './components/MainPage';
-import ProductPage from './components/ProductPage';
-import UploadPage from './components/UploadPage';
-import LoginPage from './components/LoginPage';
-import {  UploadOutlined, UserOutlined } from '@ant-design/icons';
+import MainPage from "./components/MainPage";
+import ProductPage from "./components/ProductPage";
+import UploadPage from "./components/UploadPage";
+import { UploadOutlined } from "@ant-design/icons";
 
 function App() {
-  let navigate=useNavigate();
+	let navigate=useNavigate();
 
-  return (
-    <div className="App">
-      <div id="header">
-        <div id="header-area">
-          <Link to="/">
-            <img src="/images/icons/logo.png" alt="logo" />
-          </Link>
-          <Button icon={<UserOutlined />} size='large' type='default' shape='round' onClick={()=>{navigate('/LoginPage')}}>로그인</Button>
-          <Button icon={<UploadOutlined />} size='large' type='dashed' shape='round' onClick={()=>{navigate('/UploadPage')}}>상품업로드</Button>
-        </div>
-      </div>
-      <Routes>
-        <Route path="/" element={<MainPage />}></Route>
-        <Route path="/ProductPage/:id" element={<ProductPage />}></Route>
-        <Route path="/UploadPage" element={<UploadPage />}></Route>
-        <Route path="/LoginPage" element={<LoginPage />}></Route>
-      </Routes>
-      <div id="footer">
-        <a href="#">회사소개</a>
-        <a href="#">이용약관</a>
-        <a href="#">통신판매업신고번호:123-1234</a>
-        <a href="#">사업자등록번호:456-56-78951</a>
-        <a href="#">고객센터:456-78951</a>
-      </div>
+	return (
+		<div>
+			<div id="header">
+				<div id="header-area">
+					<Link to="/">
+						<img src="/images/icons/logo.png" alt="" />
+					</Link>
+					<Button
+						icon={<UploadOutlined />}
+						size="large"
+						onClick={() => {
+							navigate('/UploadPage');
+						}}>
+						상품업로드
+					</Button>
+				</div>
+			</div>
 
-    </div>
-  );
+			<Routes>
+				<Route path="/" element={<MainPage />}></Route>
+				<Route path="/ProductPage/:id" element={<ProductPage />}></Route>
+				<Route path="/UploadPage" element={<UploadPage />}></Route>
+			</Routes>
+			<div id="footer">
+				<a href="#">회사소개</a>
+				<a href="#">이용약관</a>
+				<a href="#">통신판매업신고번호:123-1234</a>
+				<a href="#">사업자등록번호:456-56-78951</a>
+				<a href="#">고객센터:456-78951</a>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -1,56 +1,55 @@
-import './ProductPage.css';
-import React, { useState, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
-import {API_URL} from "../config/constants";
-import axios from 'axios';
-import dayjs from 'dayjs';
-import relativeTime from 'dayjs/plugin/relativeTime';
+import React, { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { API_URL } from "../config/constants.js";
+
+import axios from "axios";
+import "./ProductPage.css";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 dayjs.extend(relativeTime);
 
-function ProductPage() {
-    const { id } = useParams();
-    const [product, setProduct] = useState(null);
-    const navigate = useNavigate();
-    useEffect(() => {
-        let url = `${API_URL}products/${id}`;
-        axios
-            .get(url)
-            .then((result) => {
-                const product = result.data.product
-                console.log(product);
-                setProduct(product);
-            })
-            .catch((error) => {
-                console.log(error);
-            })
-    }, []);
-    if (product == null) {
-        return <h2>상품정보를 받고 있습니다...</h2>
-    }
-
-
-    return (
-        <div>
-            <h1 className="center">상품상세페이지</h1>
-            <div id="image-box">
-                <img src={`/${product.imageUrl}`} alt={product.name} />
-            </div>
-            <div id="profile-box">
-                <img src="/images/icons/avatar.png" alt={product.seller} />
-                <span className="product-seller">{product.seller}</span>
-            </div>
-            <div id="contents-box">
-                <div id="name">{product.name}</div>
-                <div id="price">{product.price}원</div>
-                <div id="createdAt">등록날짜: {dayjs(product.createdAt).format('YYYY-MM-DD')}</div>
-                <div id="description">{product.description}</div>
-            </div>
-            <div id="btn">
-                <button onClick={() => navigate(-1)} id="back-btn">뒤로가기</button>
-                <button onClick={() => navigate("/")} id="home-btn">홈으로</button>
-            </div>
-        </div>
-    )
-}
-
+const ProductPage = () => {
+	const navigate = useNavigate();
+	const { id } = useParams();
+	const [product, setProduct] = useState(null);
+	useEffect(() => {
+		let url = `${API_URL}/products/${id}`;
+		axios
+			.get(url)
+			.then((result) => {
+				console.log(result);
+				setProduct(result.data.product);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}, []);
+	if (product == null) {
+		return <h1>상품정보를 받고 있습니다...</h1>;
+	}
+	return (
+		<div>
+			<button
+				onClick={() => {
+					navigate(-1);
+				}}
+				id="back-btn">
+				뒤로
+			</button>
+			<div id="image-box">
+				<img src={`${API_URL}/${product.imageUrl}`} alt={product.name} />
+			</div>
+			<div id="profile-box">
+				<img src="/images/icons/avatar.png" alt={product.seller} />
+				<span className="product-seller">{product.seller}</span>
+			</div>
+			<div className="content-box">
+				<div id="name">{product.name}</div>
+				<div id="price">{product.price}</div>
+				<div className="product-date">상품등록일: {dayjs(product.createdAt).format("YY년MM월DD일-hh시MM분ss초")}</div>
+				<pre id="description">{product.description}</pre>
+			</div>
+		</div>
+	);
+};
 export default ProductPage;
